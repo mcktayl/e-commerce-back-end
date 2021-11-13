3212e2f034dc9b5e5c60b7f83720065219c68909
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-13 13:51:49 -0500

    updated columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,10 +13,20 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    tag_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
