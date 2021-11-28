1970bf611cdb63a9092ca7d57e1333a994c3b999
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-27 20:10:53 -0500

    added route to update category name

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,6 +44,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
